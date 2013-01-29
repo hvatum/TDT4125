7d8ad31a4f314284ba0b929d0834cae03d94d2ce
Stian Hvatum <stian@hvatum.net>
2013-01-29 14:13:54 +0100

    Done ex1

--- ex1/ex1.cpp
+++ ex1/ex1.cpp
@@ -14,31 +14,45 @@ using namespace std::placeholders;
 
 class Coin_counter {
     public:
-        Coin_counter(uint32_t n, vector<uint32_t>* c) : coins(c), num_coins(n) {};
+        Coin_counter(uint32_t n, vector<uint32_t>* c) : num_coins(n), coins(c) {};
         uint32_t num_coins;
         vector<uint32_t>* coins;
 };
+Coin_counter no_answer(NO_ANSWER_YET, NULL);
 
-Coin_counter* optimal(const vector<Coin_counter*>& dp, uint32_t value){
-    // TODO: Implement this function
-    return new Coin_counter(1,new vector<uint32_t>(dp[0]->coins->size(), 0));
+Coin_counter* optimal(const vector<uint32_t>& coins, const vector<Coin_counter*>& dp, uint32_t value){
+    if (dp[value] != &no_answer) return dp[value];
+    Coin_counter* smallest = &no_answer;
+    for (uint32_t i = 0; i < coins.size(); i++)
+    {
+        if ((value-coins[i]) <= value && dp[value-coins[i]] != &no_answer) {
+            Coin_counter* option = dp[value-coins[i]];
+            if (option->num_coins + 1 < smallest->num_coins)
+            {
+                smallest = new Coin_counter(option->num_coins + 1, new vector<uint32_t>(*(option->coins)));
+                uint32_t num_coins_of_type = (*(smallest->coins))[i];
+                (*(smallest->coins))[i] = num_coins_of_type + 1;
+            }
+        }
+    }
+    return smallest;
 }
 
 vector<uint32_t> calculateMinimumCoins(const vector<uint32_t>& coins, uint32_t value){
     vector<uint32_t> ret(coins.size(), 0);
 
     //Your code here
+#ifdef PRINTING
     printf("Total value is %d, and coins avaliable are:\n", value);
+#endif
     // Create DP-table with one entry for each value from 0 up until value
-    Coin_counter no_answer(NO_ANSWER_YET, NULL);
     vector<Coin_counter*> dp(value + 1, &no_answer);
 
     // Set all basis-elements to 1, since we only need one coin if value = coin-value
     vector<uint32_t >* zero = new vector<uint32_t>(coins.size(), 0);
     dp[0] = new Coin_counter(0, zero);
-    for (int i = 0; i < coins.size(); i++)
+    for (uint32_t i = 0; i < coins.size(); i++)
     {
-        printf("\t%d\n", coins[i]);
         if (coins[i] >= value) break;
         vector<uint32_t>* single_coin_answer = new vector<uint32_t>(coins.size(), 0);
         (*single_coin_answer)[i] = 1;
@@ -46,14 +60,34 @@ vector<uint32_t> calculateMinimumCoins(const vector<uint32_t>& coins, uint32_t v
 
     }
 
-    for (int current = 0; current <= value; current++)
+    // For each sub-problem, find optimal value
+    for (uint32_t current = 0; current <= value; current++)
     {
-        if (dp[current] != &no_answer) continue;
-        else dp[current] = optimal(dp, current);
+        if (dp[current] != &no_answer)
+            continue;
+        else
+            dp[current] = optimal(coins, dp, current);
     }
 
     ret = *(dp[value]->coins);
 
+#ifdef PRINTING
+    printf("Total value is %d, and coins avaliable are:\n", value);
+    printf("Using\n");
+    for (uint32_t i = 0; i < coins.size(); i++)
+    {
+        switch (ret[i])
+        {
+        case 1:
+            printf("1 coin of value %d\n", coins[i]);
+        case 0:
+            break;
+        default:
+            printf("%d coins of value %d\n", ret[i], coins[i]);
+        break;
+        }
+    }
+#endif
     return ret;
 }
 
